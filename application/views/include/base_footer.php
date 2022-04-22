d277d704b61760880c5f6a5e618b809cfc574d45
Afif <afifwoles04@gmail.com>
2022-04-22 21:42:40 +0700

    add

---
+++ application/views/include/base_footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+	<div class="float-right d-none d-sm-block">
+		<b></b>
+	</div>
+	<strong>Copyright &copy; <?php date('Y') ?> <a href="#">E-Parkir</a>.</strong> All rights
+	reserved.<a href="#" title="#" target="_blank"></a>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+	<!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
